6cc51d60a293f75abfca378ef9af54e1b5d0b793
AlexandreViry <alexandre.viry@epitech.eu>
2024-01-21 18:17:46 +0100

    [ADD] functionnal main menu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,3 +11,12 @@ func _process(delta):
 	if $AudioStreamPlayer.playing == false:
 		$AudioStreamPlayer.play()
 	pass
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+	pass # Replace with function body.
